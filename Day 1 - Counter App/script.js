7b661604e1ed788085d1699f2a17a5ec460c683f
JohnsonNYC <jkow95@gmail.com>
2021-07-08 16:52:27 -0400

    Completed Day 1

--- Day 1 - Counter App/script.js
+++ Day 1 - Counter App/script.js
@@ -25,45 +25,17 @@ function setColor(){
   }else{
     count.style.color = "white";
   }
-
-
 }
-// const count = document.querySelector(".count");
-// const add = document.querySelector(".add");
-// const subtract = document.querySelector('.subtract');
-// const reset = document.querySelector('.reset');
-// const buttons = document.querySelector('.buttons')
-// buttons.addEventListener("click", (e) => {
-//   if(e.target.classList.contains("add")){
-//     count.innerHTML++;
-//   }
-//   if(e.target.classList.contains("subtract")){
-//     count.innerHTML--;
-//   }
-//   if(e.target.classList.contains("reset")){
-//     count.innerHTML=0;
-//   }
-//   setColor();
+
+//* Created Event Delegation as shown above for buttons
+// add.addEventListener("click", () => {
+//   count.innerHTML++; 
 // })
-// // Created Event Delegation as shown above 
-// // add.addEventListener("click", () => {
-// //   count.innerHTML++; 
-// // })
 
-// // subtract.addEventListener("click", () => {
-// //   count.innerHTML--;
-// // })
+// subtract.addEventListener("click", () => {
+//   count.innerHTML--;
+// })
 
-// // reset.addEventListener("click", ()=>{
-// //   count.innerHTML = 0;
-// // })
-
-// function setColor(){
-//   if(count.innerHTML < 0 ){
-//     count.style.color = "red";
-//   }else if(count.innerHTML > 0){
-//     count.style.color = "yellow";
-//   }else{
-//     count.style.color = "white";
-//   }
-// }
+// reset.addEventListener("click", ()=>{
+//   count.innerHTML = 0;
+// })
